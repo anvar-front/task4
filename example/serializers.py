ec69f91201ad9799dd1e384692fdf2806516849c
anvar-front <69399311+anvar-front@users.noreply.github.com>
2021-02-28 00:28:10 +0300

    modified

--- example/serializers.py
+++ example/serializers.py
@@ -2,31 +2,42 @@ from rest_framework import serializers
 from example.models import Course, Contact, Branch, Category
 
 
+# class CategorySerializer(serializers.ModelSerializer):
+
+#   class Meta:
+#     model = Category
+#     fields = '__all__'
+
+
 class ContactSerializers(serializers.ModelSerializer):
-  class Meta:
-    model = Contact
-    fields = ("name", "value")
+    class Meta:
+        model = Contact
+        fields = ("name", "value")
 
 
 class BranchSerializers(serializers.ModelSerializer):
-  class Meta:
-    model = Branch
-    fields = ("latitude", "longitude", "address")
+    class Meta:
+        model = Branch
+        fields = ("latitude", "longitude", "address")
 
 
 class CourseSerializers(serializers.ModelSerializer):
-  # contact = ContactSerializers(many=True, read_only=True)
-  # branch = BranchSerializers(many=True, read_only=True)
-  category = serializers.PrimaryKeyRelatedField(queryset = Category.objects.all())
+    contacts = ContactSerializers(many=False)
+    branches = BranchSerializers(many=False)
+    # category = CategorySerializer(many=False)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
-  class Meta:
-    model = Course
-    fields = ("name", "description", "category", "logo", "contacts", "branches")
+    class Meta:
+        model = Course
+        fields = ("name", "description", "category", "logo", "contacts", "branches",)
 
-  def to_representation(self, instance):
-    data = super().to_representation(instance)
-    data['contacts'] = ContactSerializers(
-      Contact.objects.get(pk=data['contacts'])).data
-    data['branches'] = BranchSerializers(
-      Branch.objects.get(pk=data['branches'])).data
-    return data
+    def create(self, validated_data):
+        branches = validated_data.pop('branches')
+        contacts = validated_data.pop('contacts')
+        # category = validated_data.pop('category')
+        contact = Contact.objects.create(
+            **contacts)  # создание контакта с данными полученного из validated_data **распаковка
+        branch = Branch.objects.create(**branches)  # создание branch с данными полученного из validated_data
+        # category = Category.objects.create(**category) # создание категории с данными полученного из validated_data
+        course = Course.objects.create(branches=branch, contacts=contact, **validated_data)  # (category = category, branches=branch, contacts=contact, **validated_data)
+        return course
--- final/settings.py
+++ final/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
+
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -29,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'final.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
